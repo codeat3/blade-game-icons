4056457004a681dd936cba445da015a8458d5194
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-07-21 10:35:24 +0530

    added icon generation script & auto update action (#1)
    Co-authored-by: swapnilsarwe <swapnilsarwe@users.noreply.github.com>

--- src/BladeGameIconsServiceProvider.php
+++ src/BladeGameIconsServiceProvider.php
@@ -19,7 +19,6 @@ final class BladeGameIconsServiceProvider extends ServiceProvider
 
             $factory->add('game-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
-
     }
 
     private function registerConfig(): void
@@ -39,5 +38,4 @@ final class BladeGameIconsServiceProvider extends ServiceProvider
             ], 'blade-game-icons-config');
         }
     }
-
 }

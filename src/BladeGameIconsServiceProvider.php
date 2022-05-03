70ca8415dc70feffb9ecd00df95f63b4b9021f20
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-05-03 18:01:24 +0000

    Fix styling

--- src/BladeGameIconsServiceProvider.php
+++ src/BladeGameIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeGameIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-game-icons', []);
 
-            $factory->add('game-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('game-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-game-icons.php', 'blade-game-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-game-icons.php', 'blade-game-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-game-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-game-icons'),
             ], 'blade-game-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-game-icons.php' => $this->app->configPath('blade-game-icons.php'),
+                __DIR__ . '/../config/blade-game-icons.php' => $this->app->configPath('blade-game-icons.php'),
             ], 'blade-game-icons-config');
         }
     }
